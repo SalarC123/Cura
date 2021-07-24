b1db18cbbd534c62eef415b2f6812d5805f82a43
SalarC123 <68439561+SalarC123@users.noreply.github.com>
2021-07-24 17:08:59 -0500

    Backend setup (#2)
    * add proxy to backend
    
    * ignore flask and venv files
    
    * create route for getting ml model and symptoms
    
    * init pickled machine learning model
    
    * init notebook of machine learning model
    
    * fetch symptoms
    
    Co-authored-by: Salar Cheema <salar@Salars-MacBook-Air.local>

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Select from 'react-select'
+import { useEffect } from 'react'
 
 function App() {
 
@@ -8,13 +9,20 @@ function App() {
 		{ value: 'vanilla', label: 'Vanilla' }
 	]
 
+	useEffect(() => {
+		fetch("/symptoms")
+		.then(res => res.json())
+		.then(data => console.log(data))
+		.catch(err => alert(err))
+	}, [])
+
 	function getDisease(e) {
 		e.preventDefault()
 		console.log(e.target[0])
 	}
 
 	return (
-		<div className="text-white h-screen bg-red-300 flex flex-col items-center justify-center">
+		<div className="text-white h-screen bg-indigo-300 flex flex-col items-center justify-center">
 			<h1 className="text-center text-6xl font-extrabold p-4">Cura</h1>
 			<p className="w-72 md:w-96 text-center">Cura lets you gets an idea of what disease you might have based on your current symptoms. Keep in mind that this is not official medical advice and you should instead contact a liscensed doctor if you believe this prognosis is incorrect</p>
 			<form className="flex flex-col items-center p-8" onSubmit={e => getDisease(e)}>
@@ -25,7 +33,7 @@ function App() {
 					isMulti
 					id="symptoms"
 				/>
-				<input className="py-1 px-2 m-2 rounded-lg text-red-300 font-extrabold bg-white text-xl" type="submit" value="Submit" />
+				<input className="py-1 px-2 m-2 hover:bg-gray-200 rounded-lg text-indigo-300 font-extrabold bg-white text-xl" type="submit" value="Submit" />
 			</form>
 		</div>
 	);
